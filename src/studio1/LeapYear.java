94477f6b2f47bef1fe872bef65e6496d3205a2b0
Shawn <Shawn@LAPTOP-TQQ6TKU7.dhcp.wustl.edu>
2025-01-22 15:26:48 -0600

    1st commit (computer dying)

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,14 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter year: ");
+		int year = in.nextInt();		
+		boolean LeapYear = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
+		System.out.println(year + " is a leap year: " + LeapYear);
+	}
+}
